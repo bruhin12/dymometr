31c462464f07589ee01023856aaef2659254e76d
bruhin12 <thedabex@gmail.com>
2026-03-08 02:19:00 +0100

    index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 10000;
 let level = 0;
 const MAX_LEVEL = 100;
 const DECAY_SPEED = 1;       // ile % ubywa co sekundę (wolniej, żeby dym trzymał)
-const KEKW_BOOST = 5;        // ile % dodaje jedno KEKW
+const KEKW_BOOST = 2;        // ile % dodaje jedno KEKW
 const EFFECT_COOLDOWN = 5000; // 5 sekund przerwy między błyskami
 
 let lastEffectTime = 0;
@@ -95,3 +95,4 @@ connectToKick();
 server.listen(PORT, () => {
     console.log(`🚀 SERWER DZIAŁA NA PORCIE ${PORT}`);
 });
+
