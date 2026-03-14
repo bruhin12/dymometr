05d422b0a5e1ea97306e09c4985a36c200aabe77
bruhin12 <thedabex@gmail.com>
2026-03-14 21:18:23 +0100

    index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let level = 0;
 const MAX_LEVEL = 100;
 const DECAY_SPEED = 2;       // ile % ubywa co sekundę (wolniej, żeby dym trzymał)
 const KEKW_BOOST = 0.6;        // ile % dodaje jedno KEKW
-const EFFECT_COOLDOWN = 300000; // 5 sekund przerwy między błyskami
+const EFFECT_COOLDOWN = 600000; // 5 sekund przerwy między błyskami
 
 let lastEffectTime = 0;
 
